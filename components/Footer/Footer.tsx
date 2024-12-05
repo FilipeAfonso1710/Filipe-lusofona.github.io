26cd0b4c961a6f25ed1deda8610bdb02d2ce7177
filipeafonso1710 <a22304588@alunos.ulht.pt>
2024-12-05 11:36:37 +0000

    lab11

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return <footer className="bg-none text-white font-bold" p-2 text-center>
+    2024© Filipe Afonso DIW 
+  </footer>
+}
---
+++ components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return <header>
+    Lab 11: Next e
+  </header>
+}
