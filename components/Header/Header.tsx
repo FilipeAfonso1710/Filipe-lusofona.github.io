8df5ab0bbe625cf02e218dd2b8139cc98e36ed9e
filipeafonso1710 <a22304588@alunos.ulht.pt>
2024-12-05 12:00:27 +0000

    lab 11

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,7 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Header() {
-  return <header>
-    Lab 11: Next e
+  return (
+  <header className="p-5 bd-none text-red font-bold">
+    <h1 className='mb-3 text-center'>
+        Lab 11: React & Next.js</h1>
+    <nav className='max-w-screen-lg display-flex flex-row justify-center gap-30'>
+        <Link href="/" className='text-white'>
+        Home</Link>
+    </nav>
   </header>
+  )
 }
